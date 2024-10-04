13bf61afbdf9dd450e86a36c10506569d415104c
Bhargav Guggilapu <bhargavguggilapu@gmail.com>
2024-10-03 23:34:27 -0500

    added vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3001, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is running on port 3001");
     });
   })
